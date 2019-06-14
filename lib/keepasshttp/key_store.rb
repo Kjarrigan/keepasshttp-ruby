1bd8f7e92e8c8ed9f8cac008b2fa9b6a0c17f63a
Kjarrigan <keepasshttp-ruby@kjarrigan.de>
2019-06-14 13:47:24 +0200

    Enhance KeyStore handling
    * Add Environment
    * Add :auto (which is the new default) to try to guess the key_store

--- lib/keepasshttp/key_store.rb
+++ lib/keepasshttp/key_store.rb
@@ -3,11 +3,44 @@
 require 'yaml'
 
 class Keepasshttp
+  # Manage keepasshttp credentials by storing them in various places to reuse
+  # them and prevent popping up keepass dialogs
   module KeyStore
+    using Base64Helper
+
+    def self.list
+      constants.sort.map do |name|
+        klass = const_get(name)
+        next nil unless klass.is_a?(Class)
+
+        klass
+      end.compact
+    end
+
+    def self.init(type)
+      case type
+      when Hash, String then External.new(key_store)
+      when false then nil
+      when :auto
+        use = list.find(&:available?)
+        use ||= Environment
+        use
+      else KeyStore.const_get(type)
+      end
+    rescue StandardError
+      raise ArgumentError, "Invalid key_store #{type.inspect}"
+    end
+
     # Input the key and the id directly into the client so you can take care
     # of the storage yourself.
     class External
-      def initialize(key:, id:)
+      def initialize(token = nil, key: nil, id: nil)
+        if token && key
+          raise ArgumentError, 'Either provide a ID:KEY string or a hash, not' \
+                               ' both'
+        end
+        key, id = string.split(':') if token.is_a?(String)
+
         @params = { key: key, id: id }
       end
 
@@ -22,13 +55,37 @@ class Keepasshttp
       def available?
         true
       end
+
+      def self.available?
+        false
+      end
+    end
+
+    # Read id and key from the KEEPASSHTTP_TOKEN environment variable
+    # you can even use this to store the values but this obviously only
+    # survives within the running ruby process
+    class Environment
+      class << self
+        def load
+          id, key = ENV['KEEPASSHTTP_TOKEN'].split(':')
+          { key: key.from_base64, id: id }
+        end
+
+        def save(id:, key:, **_params)
+          ENV['KEEPASSHTTP_TOKEN'] = [id, key.to_base64].join(':')
+        end
+
+        def available?
+          !ENV['KEEPASSHTTP_TOKEN'].nil?
+        end
+      end
     end
 
     # The most simple but unsecure way wo store your session key (so you don't
     # have to reenter the label over and over again). Use this only for testing!
     class Plain
       # TODO, make the PATH adjustable
-      PATH = File.join(Dir.home, '.keepasshttp-ruby')
+      PATH = File.join(Dir.home, '.config', 'keepasshttp-ruby')
       def self.save(params = {})
         File.write(PATH, params.to_yaml)
       end
@@ -48,7 +105,7 @@ class Keepasshttp
         def available?
           require 'net/ssh'
 
-          super
+          super && File.read(Plain::PATH).match?(/iv:/)
         rescue LoadError
           raise LoadError, 'To use key_store: :SshAgent you have to install ' \
                            "the 'net-ssh' gem"
--- spec/keepasshttp_spec.rb
+++ spec/keepasshttp_spec.rb
@@ -1,40 +1,50 @@
 # frozen_string_literal: true
 
+Keepasshttp::KeyStore::Plain::PATH = "/tmp/keepasshttp.test.#{rand(1_000_000)}"
+
 RSpec.describe Keepasshttp do
   it 'has a version number' do
     expect(Keepasshttp::VERSION).not_to be nil
   end
 
   context 'Keepasshttp::KeyStore' do
-    it 'has a External format' do
-      ext = Keepasshttp::KeyStore::External.new(id: 'Foo', key: '1234567890')
-
-      expect(ext.available?).to be(true)
-      expect(ext.save(id: 'Bar', key: '0987654321')).to be(false)
-      expect(ext.load).to eq(
-        id: 'Foo', key: '1234567890'
-      )
+    let :credentials do
+      { id: 'Foo', key: '1234567890' }
     end
 
-    it 'has a Plain format' do
-      expect(Keepasshttp::KeyStore::Plain.available?).to be(true)
-      Keepasshttp::KeyStore::Plain.save(id: 'Foo', key: '1234567890')
+    let :changed_credentials do
+      { id: 'Bar', key: '0987654321' }
+    end
+
+    it 'has a External store' do
+      store = Keepasshttp::KeyStore::External
+      expect(store.available?).to be(false)
+
+      store = store.new(credentials)
+
+      expect(store.save(changed_credentials)).to be(false)
+      expect(store.load).to eq(credentials)
+    end
+
+    it 'has a Plain store' do
+      store = Keepasshttp::KeyStore::Plain
+      expect(store.available?).to be(false)
+      store.save(credentials)
+      expect(store.available?).to be(true)
 
       expect(File.read(Keepasshttp::KeyStore::Plain::PATH)).to eq(<<~YAML)
         ---
         :id: Foo
         :key: '1234567890'
       YAML
-
-      expect(Keepasshttp::KeyStore::Plain.load).to eq(
-        id: 'Foo', key: '1234567890'
-      )
+      expect(store.load).to eq(credentials)
     end
 
-    it 'has an SshAgent format (encrypted Plain)' do
-      expect(Keepasshttp::KeyStore::SshAgent.available?).to be(true)
-
-      Keepasshttp::KeyStore::SshAgent.save(id: 'Foo', key: '1234567890')
+    it 'has an SshAgent store (encrypted Plain)' do
+      store = Keepasshttp::KeyStore::SshAgent
+      expect(store.available?).to be(false)
+      store.save(credentials)
+      expect(store.available?).to be(true)
 
       cfg_file = File.read(Keepasshttp::KeyStore::Plain::PATH)
       expect(cfg_file).to match(/id: Foo/)
@@ -42,15 +52,26 @@ RSpec.describe Keepasshttp do
       expect(cfg_file).to_not match(/1234567890/)
       expect(cfg_file).to match(/iv:/)
 
-      expect(Keepasshttp::KeyStore::SshAgent.load).to eq(
-        id: 'Foo', key: '1234567890'
-      )
+      expect(store.load).to eq(credentials)
+    end
+
+    it 'has an Environment store' do
+      store = Keepasshttp::KeyStore::Environment
+
+      expect(store.available?).to be(false)
+      ENV['KEEPASSHTTP_TOKEN'] = 'Foo:MTIzNDU2Nzg5MA=='
+      expect(store.available?).to be(true)
+
+      expect(store.load).to eq(credentials)
+
+      store.save(changed_credentials)
+      expect(store.load).to eq(changed_credentials)
     end
   end
 
   context 'Client' do
     it 'can login to an opened Keepass' do
-      kee = Keepasshttp.connect
+      kee = Keepasshttp.connect(key_store: false)
       expect(kee.id).to eq('Test')
     end
 
